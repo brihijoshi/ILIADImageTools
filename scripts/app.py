9f6a2dbed9cf813d24552d0842008561ad700437
Brihi <brihi16142@iiitd.ac.in>
2019-04-12 01:31:25 +0530

    App basics work

--- scripts/app.py
+++ scripts/app.py
@@ -12,6 +12,9 @@ from kivy.clock import Clock
 from kivy.uix.filechooser import FileChooserListView 
 from kivy.uix.popup import Popup
 from kivy.uix.image import Image
+import matplotlib.image as mpimg
+import numpy as np
+from skimage import io
 from functools import wraps
 import mido
 import random
@@ -25,21 +28,23 @@ title_label = Label(text="Rate of transfer (delay) in secs: 5",
 				  size_hint=(.2, .2))
 
 send_slider = Slider(min=0, max=5, value=25, pos_hint={'x': 0.5, 'y': 0.4})
-send_button = Button(text='Send', font_size=14, pos_hint={'x': 0.85, 'y': 0.15},size_hint=(0.15, 0.15))
+send_button = Button(text='Send', font_size=14, pos_hint={'x': 0.85, 'y': 0.1},size_hint=(0.15, 0.15))
 
 file_selector = Button(text = 'Select Image', pos_hint={'x': 0.01, 'y': 0.2},size_hint=(0.15, 0.15))
 
 
 outport = mido.open_output('New', virtual=True, autoreset=True)
 run_dict = {'v':5}
+# global image_file
 
 
-def send_RGB(r):
+def send_RGB(r, g, b):
+	print('ENTERED!!!!')
 
 	# Just sending the MIDO messages
 	outport.send(mido.Message('control_change', channel=1-1, control=16, value=r))
-	# outport.send(mido.Message('control_change', channel=1+1, control=17, value=g))
-	# outport.send(mido.Message('control_change', channel=1+1, control=17, value=b))
+	outport.send(mido.Message('control_change', channel=1-1, control=17, value=g))
+	outport.send(mido.Message('control_change', channel=1-1, control=18, value=b))
 
 
 def yield_to_sleep(func):
@@ -59,12 +64,18 @@ def yield_to_sleep(func):
 
 @yield_to_sleep  # use this decorator to cast 'yield' to non-blocking sleep
 def read_image():
-	for i in range(10000):
-		yield run_dict["v"]  # use yield to "sleep"
-		r = random.randint(10, 90)
-		# g = random.randint(, 40)
-		# b = random.randint(30, 50)
-		send_RGB(r)
+	print('ENTERED READ IMAGE')
+	# img = mpimg.imread(image_chosen_path)
+	# print('IMAGE READ')
+	print(image_file)
+	for i in range(len(image_file)):
+		for j in range(len(image_file[0])):
+			yield run_dict["v"]  # use yield to "sleep"
+			r = int(image_file[i,j,0])
+			g = int(image_file[i,j,1])
+			b = int(image_file[i,j,2])
+			print(r,g,b)
+			send_RGB(r,g,b)
 
 
 def OnSliderValueChange(instance,value):
@@ -72,7 +83,11 @@ def OnSliderValueChange(instance,value):
 	print(value, run_dict['v'])
 
 def OnButtonPressed(instance):
-	print('Button Pressed!!!!!')
+	print('ABOVE')
+	print(image_chosen_path)
+	print('Button Pressed')
+	print(type(image_file))
+	read_image()
 
 
 class app(App):
@@ -108,6 +123,7 @@ class app(App):
 		self.filechooserpopup.open()
 
 	def select_image_file_path(self, instance):
+		global image_chosen_path, image_file
 		# videofilepath = self.filechooserview.selection
 		print(self.filechooserview.selection)
 		if len(self.filechooserview.selection) == 0:
@@ -120,10 +136,12 @@ class app(App):
 			content.add_widget(btn)
 			popup.open()
 		else:
-			self.image_chosen_path = self.filechooserview.selection
+			image_chosen_path = self.filechooserview.selection[0]
+			image_file = mpimg.imread(image_chosen_path)
+			print(image_chosen_path,'HELLLOOOO')
 			# videofilepath = self.filechooserview.selection[0]
 			self.filechooserpopup.dismiss()
-			disp_img = Image(source=self.image_chosen_path[0],pos_hint={'x': 0.01, 'y': 0.5},keep_ratio=True, size_hint=(0.5,0.5))
+			disp_img = Image(source=image_chosen_path,pos_hint={'x': 0.01, 'y': 0.5},keep_ratio=True, size_hint=(0.5,0.5))
 			FLOAT_LAYOUT.add_widget(disp_img)
 			# stopped = 2
 			# self.file_path_text.text = '...' + videofilepath[-50:]
