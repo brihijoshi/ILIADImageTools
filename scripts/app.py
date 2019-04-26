1f76ca16ef37cdf2d0bb4f46e8c650a3cd4530cd
Brihi <brihi16142@iiitd.ac.in>
2019-04-27 02:26:25 +0530

    Adding colour features to image

--- scripts/app.py
+++ scripts/app.py
@@ -15,9 +15,12 @@ from kivy.uix.image import Image
 import matplotlib.image as mpimg
 import numpy as np
 from skimage import io
+from skimage.color import convert_colorspace, rgba2rgb, hsv2rgb
 from functools import wraps
 import mido
 import random
+
+TEMP_PATH = "../assets/"
 
 
 FLOAT_LAYOUT = FloatLayout(size=(300, 300))
@@ -27,13 +30,40 @@ title_label = Label(text="Rate of transfer (delay) in secs: 5",
 				  pos_hint={'x': .4, 'y': .8},
 				  size_hint=(.2, .2))
 
-send_slider = Slider(min=0, max=5, value=25, pos_hint={'x': 0.5, 'y': 0.4})
-send_button = Button(text='Send', font_size=14, pos_hint={'x': 0.85, 'y': 0.1},size_hint=(0.15, 0.15))
+speed_slider = Slider(min=0,
+		 max=10, 
+		 value=0.1,
+		 step = 1,
+		 pos_hint={'x': 0.5, 'y': 0.5},
+		 size_hint=(.5, .8)
+		 )
 
-file_selector = Button(text = 'Select Image', pos_hint={'x': 0.01, 'y': 0.2},size_hint=(0.15, 0.15))
+speed_slider_label = Label(text='Time Delay (in seconds) : ', font_size=15, pos_hint={'x': 0.47, 'y': 0.55}, size_hint=(.5, .8))
+speed_slider_value_label = Label(text='0.0', font_size=15, pos_hint={'x': 0.70, 'y': 0.55}, size_hint=(.3, .8), bold=True)
 
 
-outport = mido.open_output('New', virtual=True, autoreset=True)
+# speed_slider = Slider(min=0, max=1, value=0.5, pos_hint={'x': 0.5, 'y': 0.4}, padding=2)
+send_button = Button(text='Send', font_size=14, pos_hint={'x': 0.87, 'y': 0.05},size_hint=(0.12, 0.07))
+# stop_button = Button(text='Stop', font_size=14, pos_hint={'x': 0.74, 'y': 0.05},size_hint=(0.12, 0.07))
+send_status_label = Label(text='Press Send', font_size=15, pos_hint={'x': 0.87, 'y': 0.01}, size_hint=(0.12, 0.04), bold=True)
+
+file_selector = Button(text = 'Select Image', pos_hint={'x': 0.01, 'y': 0.50},size_hint=(0.12, 0.07))
+
+
+"""Colour Conversions"""
+
+hue_slider = Slider(min=0,
+		 max=128, 
+		 value=0,
+		 step = 1,
+		 pos_hint={'x': 0.2, 'y': 0.1},
+		 size_hint=(.5, .5)
+		 )
+
+
+
+
+outport = mido.open_output('ILIAD - ImgTools', virtual=True, autoreset=True)
 run_dict = {'v':5}
 # global image_file
 
@@ -76,18 +106,68 @@ def read_image():
 			b = int(image_file[i,j,2])
 			print(r,g,b)
 			send_RGB(r,g,b)
+	send_status_label.text = 'Finished!'
+	send_status_label.color = [0,128,0,1]
 
 
-def OnSliderValueChange(instance,value):
+
+def OnSpeedSliderValueChange(instance,value):
 	run_dict["v"] = value
+	speed_slider_value_label.text = str(value)
 	print(value, run_dict['v'])
 
-def OnButtonPressed(instance):
-	print('ABOVE')
-	print(image_chosen_path)
-	print('Button Pressed')
-	print(type(image_file))
+
+def OnSendButtonPressed(instance):
+	send_status_label.text = 'Sending...'
+	send_status_label.color = [128,0,0,1]
+	# print('ABOVE')
+	# print(image_chosen_path)
+	print('Send Button Pressed')
+	# print(type(image_file))
 	read_image()
+
+def OnHueSliderValueChange(instance, value):
+	# print('ENTERED')
+	# image_shape = image_file.shape
+	# image_file_rgb = ""
+	# if image_shape[2] == 4:
+	# 	image_file_rgb = rgba2rgb(image_file)
+	# elif image_shape[2] == 3:
+	# 	image_file_rgb = image_file
+
+
+	# image_file_hsv = convert_colorspace(image_file_rgb, 'RGB', 'HSV')
+
+	# print(image_file_hsv[:,:,0])
+
+	# image_file_hsv[:,:,0] = image_file_hsv[:,:,0] + value
+
+	# print(image_file_hsv[:,:,0])
+
+	# np.clip(image_file_hsv,0,1,out=image_file_hsv)
+
+	# converted_rgb = hsv2rgb(image_file_hsv)
+
+	# io.imsave(TEMP_PATH+'temp.jpg',converted_rgb)
+
+	# disp_img.source = TEMP_PATH+'temp.jpg'
+
+	# print(disp_img.source)
+
+	# print(image_file_hsv.shape)
+
+	# print('Conversion done')
+
+	disp_img.color = [value, disp_img.color[1], disp_img.color[2],disp_img.color[3]]
+
+
+
+
+
+# def OnStopButtonPressed(instance):
+# 	outport.reset()
+# 	send_status_label.text = 'Stopped'
+# 	send_status_label.color = [255,0,0,1]
 
 
 class app(App):
@@ -141,21 +221,44 @@ class app(App):
 			print(image_chosen_path,'HELLLOOOO')
 			# videofilepath = self.filechooserview.selection[0]
 			self.filechooserpopup.dismiss()
-			disp_img = Image(source=image_chosen_path,pos_hint={'x': 0.01, 'y': 0.5},keep_ratio=True, size_hint=(0.5,0.5))
-			FLOAT_LAYOUT.add_widget(disp_img)
-			# stopped = 2
-			# self.file_path_text.text = '...' + videofilepath[-50:]
+			# FLOAT_LAYOUT.remove_widget(disp_img)
+			disp_img.source = image_chosen_path
+
+			speed_slider.disabled = False
+			send_button.disabled = False
+			hue_slider.disabled = False
 
 
 	def build(self):
-		
-		FLOAT_LAYOUT.add_widget(file_selector)
-		FLOAT_LAYOUT.add_widget(send_slider)
-		FLOAT_LAYOUT.add_widget(send_button)
 
-		send_slider.bind(value=OnSliderValueChange)
-		send_button.bind(on_press=OnButtonPressed)
+		global disp_img
+
+		disp_img = Image(source='../assets/init_back.jpg',pos_hint={'x': 0.01, 'y': 0.58},keep_ratio=True, size_hint=(0.45,0.45))
+		FLOAT_LAYOUT.add_widget(disp_img)
+		FLOAT_LAYOUT.add_widget(file_selector)
+
+		FLOAT_LAYOUT.add_widget(speed_slider)
+		FLOAT_LAYOUT.add_widget(speed_slider_label)
+		FLOAT_LAYOUT.add_widget(speed_slider_value_label)
+
+
+		FLOAT_LAYOUT.add_widget(send_button)
+		# FLOAT_LAYOUT.add_widget(stop_button)
+		FLOAT_LAYOUT.add_widget(send_status_label)
+
+		FLOAT_LAYOUT.add_widget(hue_slider)
+
+
+		speed_slider.bind(value=OnSpeedSliderValueChange)
+		send_button.bind(on_press=OnSendButtonPressed)
+		# stop_button.bind(on_press=OnStopButtonPressed)
 		file_selector.bind(on_press = self.create_popup)
+
+		hue_slider.bind(value=OnHueSliderValueChange)
+
+		speed_slider.disabled = True
+		send_button.disabled = True
+		hue_slider.disabled = True
 		
 		return FLOAT_LAYOUT
 
