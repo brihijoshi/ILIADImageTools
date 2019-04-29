2da845a113b8d29ce879793beedcb570a32e69b4
Brihi <brihi16142@iiitd.ac.in>
2019-04-30 01:50:53 +0530

    Added swirl, improved GUI, everything works till now

--- scripts/app.py
+++ scripts/app.py
@@ -16,12 +16,15 @@ from kivy.uix.checkbox import CheckBox
 from kivy.graphics.texture import Texture
 from kivy.core.image import Image as CoreImage
 from kivy.uix.spinner import Spinner
+from kivy.graphics import *
+from skimage.transform import swirl
+from skimage import util
 from io import BytesIO
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import io, segmentation
-from skimage.color import convert_colorspace, rgba2rgb, hsv2rgb, rgb2gray, label2rgb, rgb2hsv
+from skimage.color import convert_colorspace, rgba2rgb, hsv2rgb, rgb2gray, label2rgb, rgb2hsv, gray2rgb
 from skimage.future import graph
 from PIL import Image as PImage
 from functools import wraps
@@ -31,7 +34,6 @@ import random
 import copy
 
 
-
 outport = mido.open_output('ILIAD - ImgTools', virtual=True, autoreset=True)
 run_dict = {'v':0}
 image_file_dict = {'image_file':"", 'orig_image':""}
@@ -39,16 +41,10 @@ channel_dict = {'channel':1}
 cc_dict = {'cc':16}
 
 
-
 TEMP_PATH = "../assets/"
 
 
 FLOAT_LAYOUT = FloatLayout(size=(300, 300))
-
-title_label = Label(text="Rate of transfer (delay) in secs: 5",
-				  font_size=20,
-				  pos_hint={'x': .4, 'y': .8},
-				  size_hint=(.2, .2))
 
 speed_slider = Slider(min=0,
 		 max=10, 
@@ -58,14 +54,14 @@ speed_slider = Slider(min=0,
 		 size_hint=(.5, .8)
 		 )
 
-speed_slider_label = Label(text='Time Delay (in seconds) : ', font_size=15, pos_hint={'x': 0.47, 'y': 0.55}, size_hint=(.5, .8))
-speed_slider_value_label = Label(text='0.0', font_size=15, pos_hint={'x': 0.70, 'y': 0.55}, size_hint=(.3, .8), bold=True)
+speed_slider_label = Label(text='Time Delay (in seconds) : ', font_size=17, pos_hint={'x': 0.47, 'y': 0.55}, size_hint=(.5, .8))
+speed_slider_value_label = Label(text='0.0', font_size=17, pos_hint={'x': 0.70, 'y': 0.55}, size_hint=(.3, .8), bold=True)
 
 
 # speed_slider = Slider(min=0, max=1, value=0.5, pos_hint={'x': 0.5, 'y': 0.4}, padding=2)
 send_button = Button(text='Send', font_size=14, pos_hint={'x': 0.87, 'y': 0.05},size_hint=(0.12, 0.07))
 # stop_button = Button(text='Stop', font_size=14, pos_hint={'x': 0.74, 'y': 0.05},size_hint=(0.12, 0.07))
-send_status_label = Label(text='Press Send', font_size=15, pos_hint={'x': 0.87, 'y': 0.01}, size_hint=(0.12, 0.04), bold=True)
+send_status_label = Label(text='Press Send', font_size=17, pos_hint={'x': 0.87, 'y': 0.01}, size_hint=(0.12, 0.04), bold=True)
 
 file_selector = Button(text = 'Select Image', pos_hint={'x': 0.01, 'y': 0.50},size_hint=(0.12, 0.07))
 
@@ -73,27 +69,55 @@ reset_button = Button(text='Reset', font_size=14, pos_hint={'x': 0.87, 'y': 0.15
 
 # Buttons for the colour preference image-
 
-cp_coloured_button = CheckBox()
-cp_coloured_button.text = 'Coloured'
-cp_coloured_button.pos_hint = {'x': 0.07, 'y': 0.05}
-cp_coloured_button.size_hint = (0.12, 0.07)
-cp_coloured_button.group = 'colour_pref'
-cp_coloured_button.active = True
-cp_coloured_button.color = [128, 128, 128, 1]
-cp_grayscale_button = CheckBox()
-cp_grayscale_button.text = 'Gray Scale'
-cp_grayscale_button.pos_hint = {'x': 0.27, 'y': 0.05}
-cp_grayscale_button.size_hint = (0.12, 0.07)
-cp_grayscale_button.group = 'colour_pref'
-cp_grayscale_button.active = False
-cp_grayscale_button.color = [128, 128, 128, 1]
 cp_inverted_button = CheckBox()
-cp_inverted_button.text = 'Inverted'
-cp_inverted_button.pos_hint = {'x': 0.47, 'y': 0.05}
-cp_inverted_button.size_hint = (0.12, 0.07)
+cp_inverted_button.pos_hint = {'x': 0.02, 'y': 0.20}
+cp_inverted_button.size_hint = (0.05, 0.17)
 cp_inverted_button.group = 'colour_pref'
 cp_inverted_button.active = False
 cp_inverted_button.color = [128, 128, 128, 1]
+cp_grayscale_button = CheckBox()
+cp_grayscale_button.pos_hint = {'x': 0.12, 'y': 0.20}
+cp_grayscale_button.size_hint = (0.05, 0.17)
+cp_grayscale_button.group = 'colour_pref'
+cp_grayscale_button.active = False
+cp_grayscale_button.color = [128, 128, 128, 1]
+cp_animated_button = CheckBox()
+cp_animated_button.pos_hint = {'x': 0.22, 'y': 0.20}
+cp_animated_button.size_hint = (0.05, 0.17)
+cp_animated_button.group = 'colour_pref'
+cp_animated_button.active = False
+cp_animated_button.color = [128, 128, 128, 1]
+
+colour_filter_label = Label(text='Colour Filters', font_size=17, pos_hint={'x': 0.12, 'y': 0.26}, size_hint=(.05, .17))
+invert_filter_label = Label(text='Invert', font_size=14, pos_hint={'x': 0.02, 'y': 0.15}, size_hint=(.05, .17))
+grayscale_filter_label = Label(text='Grayscale', font_size=14, pos_hint={'x': 0.12, 'y': 0.15}, size_hint=(.05, .17))
+animate_filter_label = Label(text='Animate', font_size=14, pos_hint={'x': 0.22, 'y': 0.15}, size_hint=(.05, .17))
+
+
+transformation_label = Label(text='Transform', font_size=17, pos_hint={'x': 0.37, 'y': 0.26}, size_hint=(.05, .17))
+transform_dropdown = Spinner(
+	# default value shown
+	text='Select Transform',
+	# available values
+	values=('Swirl','Nothing'),
+	# just for positioning in our example
+	pos_hint={'x': 0.32, 'y': 0.21}, size_hint=(.15, .10))
+
+def OnTransformDropdownSelect(spinner, text):
+	if text=='Swirl':
+
+		image_to_swirl = image_file_dict['image_file']
+
+		swirled = swirl(image_to_swirl, rotation=0, strength=10, radius=400)
+
+		image_file_dict['image_file'] = swirled
+
+		
+		image_texture = Texture.create(size=(swirled.shape[1], swirled.shape[0]),colorfmt='rgb')
+		image_texture.blit_buffer(np.float32(np.flip(swirled,axis=0).ravel()), colorfmt='rgb', bufferfmt='float')
+		disp_img.texture = image_texture
+
+
 
 channel_values = []
 for i in range(1,17):
@@ -121,23 +145,35 @@ channel_dropdown = Spinner(
 	# available values
 	values=tuple(channel_values),
 	# just for positioning in our example
-	pos_hint={'x': 0.50, 'y': 0.75}, size_hint=(.2, .1))
+	pos_hint={'x': 0.50, 'y': 0.65}, size_hint=(.2, .1))
 
-def OnChannelDropdownSelect(spinner, text):
-	channel_dict['channel'] = int(text.split(" ")[-1])
-	print(channel_dict['channel'])
+channel_label = Label(text='Channel', font_size=17, pos_hint={'x': 0.50, 'y': 0.75}, size_hint=(.2, .1))
+
 
 
 hue_slider = Slider(min=0,
 		 max=1, 
 		 value= 0,
 		 step = 0.01,
-		 pos_hint={'x': 0.02, 'y': 0.25},
-		 size_hint=(.23, .3)
+		 pos_hint={'x': 0.02, 'y': 0.40},
+		 size_hint=(.23, .01)
 		 )
 
-speed_slider_label = Label(text='Time Delay (in seconds) : ', font_size=15, pos_hint={'x': 0.47, 'y': 0.55}, size_hint=(.5, .8))
-speed_slider_value_label = Label(text='0.0', font_size=15, pos_hint={'x': 0.70, 'y': 0.55}, size_hint=(.3, .8), bold=True)
+hue_slider_label = Label(text='Hue', font_size=17, pos_hint={'x': 0.02, 'y': 0.45}, size_hint=(.23, .01))
+
+saturation_slider = Slider(min=0,
+		 max=1, 
+		 value= 0,
+		 step = 0.01,
+		 pos_hint={'x': 0.25, 'y': 0.40},
+		 size_hint=(.23, .01)
+		 )
+
+saturation_slider_label = Label(text='Saturation', font_size=17, pos_hint={'x': 0.25, 'y': 0.45}, size_hint=(.23, .01))
+
+
+
+
 
 
 def send_MIDI(r, g=None, b=None):
@@ -182,10 +218,10 @@ def read_image():
 	print((temp == image_file_dict['orig_image']))
 	if len(temp.shape) == 3:
 		if (temp == image_file_dict['orig_image']).all() == False:
-			temp = temp * 100
+			temp = temp * 255
 	elif len(temp.shape) == 2:
 		if temp != image_file_dict['orig_image']:
-			temp = temp * 100
+			temp = temp * 255
 
 	
 	print(temp)
@@ -225,10 +261,32 @@ def OnSendButtonPressed(instance):
 def OnResetButtonPressed(instance):
 
 	disp_img.texture = orig_image_texture
+	cp_inverted_button.active = False
+	cp_grayscale_button.active = False
+	cp_animated_button.active = False
+	transform_dropdown.text = 'Select Transform'
+	hue_slider.value = 0
+	saturation_slider.value = 0
+	image_file_dict['image_file'] = io.imread(image_chosen_path)
 
 
-def OnCPColouredButtonPressed(instance, value):
-	print('Pressed colour')
+def OnCPInvertedButtonPressed(instance, value):
+	print('Pressed inverted')
+	print('ENTERED')
+	image_shape = image_file.shape
+	print(image_file.shape)
+
+	image_file_inverted = util.invert(np.flip(image_file,axis=0)).ravel()/255
+
+
+	image_file_dict['image_file'] = util.invert(image_file)/255
+
+	# # buf1 = cv2.flip(image, 0)
+	# buf = image_file_gray.tostring()
+	# arr = array('B', image_file_gray)
+	image_texture = Texture.create(size=(image_file.shape[1], image_file.shape[0]),colorfmt='rgb')
+	image_texture.blit_buffer(np.float32(image_file_inverted), colorfmt='rgb', bufferfmt='float')
+	disp_img.texture = image_texture
 
 def OnCPGrayscaleButtonPressed(instance, value):
 	print('Pressed Grayscale')
@@ -260,8 +318,8 @@ def OnCPGrayscaleButtonPressed(instance, value):
 	# im = CoreImage(BytesIO(data.read()), ext='png')
 	# disp_img.texture = im.texture
 
-def OnCPInvertedButtonPressed(instance, value):
-	print('Pressed Inverted')
+def OnCPanimatedButtonPressed(instance, value):
+	print('Pressed animated')
 
 
 	def _weight_mean_color(graph, src, dst, n):
@@ -306,30 +364,30 @@ def OnCPInvertedButtonPressed(instance, value):
 		graph.node[dst]['pixel count'] += graph.node[src]['pixel count']
 		graph.node[dst]['mean color'] = (graph.node[dst]['total color'] /
 										 graph.node[dst]['pixel count'])
-	image_file_inverted = image_file
+	image_file_animated = image_file
 
-	labels = segmentation.slic(image_file_inverted, compactness=30, n_segments=400)
-	g = graph.rag_mean_color(image_file_inverted, labels)
+	labels = segmentation.slic(image_file_animated, compactness=30, n_segments=400)
+	g = graph.rag_mean_color(image_file_animated, labels)
 
 	labels2 = graph.merge_hierarchical(labels, g, thresh=35, rag_copy=False,
 									   in_place_merge=True,
 									   merge_func=merge_mean_color,
 									   weight_func=_weight_mean_color)
 
-	out = label2rgb(labels2, image_file_inverted, kind='avg')
+	out = label2rgb(labels2, image_file_animated, kind='avg')
 	out = segmentation.mark_boundaries(out, labels2, (0, 0, 0))
 
 	image_file_dict['image_file'] = out
 
 	print(out)
 
-	# image_file_inverted = np.float32(image_file_inverted)
+	# image_file_animated = np.float32(image_file_animated)
 
-	image_texture = Texture.create(size=(image_file_inverted.shape[1], image_file_inverted.shape[0]),colorfmt='rgb')
+	image_texture = Texture.create(size=(image_file_animated.shape[1], image_file_animated.shape[0]),colorfmt='rgb')
 	image_texture.blit_buffer(np.flip(np.float32(out),axis=0).ravel(), colorfmt='rgb', bufferfmt='float')
 	disp_img.texture = image_texture
 
-	print("DONE Inverted")
+	print("DONE animated")
 
 def TintImageHue(hue):
 	""" Add color of the given hue to an RGB image.
@@ -338,13 +396,31 @@ def TintImageHue(hue):
 	"""
 	temp = image_file_dict['image_file']
 
+	print('--------------- HUE ---------------')
 	print(temp)
 
-	hsv = rgb2hsv(temp)
-	hsv[:, :, 0] = hue
+	if len(temp.shape) == 3:
 
-	print(hsv2rgb(hsv))
-	return hsv2rgb(hsv)
+
+		hsv = rgb2hsv(temp)
+		hsv[:, :, 0] = hue
+
+		print(hsv2rgb(hsv))
+
+		print('--------------- HUE ---------------')
+		return hsv2rgb(hsv)
+
+	elif len(temp.shape) == 2:
+
+		rgb = gray2rgb(temp)
+		hsv = rgb2hsv(rgb)
+		hsv[:, :, 0] = hue
+
+		print(hsv2rgb(hsv))
+
+		print('--------------- HUE ---------------')
+		return hsv2rgb(hsv)
+
 
 def OnHueSliderChange(instance,value):
 	temp = TintImageHue(value)
@@ -354,6 +430,42 @@ def OnHueSliderChange(instance,value):
 	image_texture = Texture.create(size=(temp.shape[1], temp.shape[0]),colorfmt='rgb')
 	image_texture.blit_buffer(np.flip(np.float32(temp),axis=0).ravel(), colorfmt='rgb', bufferfmt='float')
 	disp_img.texture = image_texture
+
+def TintImageSaturation(saturation):
+	""" Add color of the given hue to an RGB image.
+
+	By default, set the saturation to 1 so that the colors pop!
+	"""
+	temp = image_file_dict['image_file']
+
+	print(temp)
+
+	if len(temp.shape) == 3:
+
+		hsv = rgb2hsv(temp)
+		hsv[:, :, 1] = saturation
+
+		print(hsv2rgb(hsv))
+		return hsv2rgb(hsv)
+	elif len(temp.shape) == 2:
+
+		rgb = gray2rgb(temp)
+		hsv = rgb2hsv(rgb)
+		hsv[:, :, 1] = saturation
+
+		print(hsv2rgb(hsv))
+		return hsv2rgb(hsv)
+
+
+def OnSaturationSliderChange(instance,value):
+	temp = TintImageSaturation(value)
+	image_file_dict['image_file'] = temp
+	print(image_file_dict['image_file'])
+
+	image_texture = Texture.create(size=(temp.shape[1], temp.shape[0]),colorfmt='rgb')
+	image_texture.blit_buffer(np.flip(np.float32(temp),axis=0).ravel(), colorfmt='rgb', bufferfmt='float')
+	disp_img.texture = image_texture
+
 
 
 
@@ -424,6 +536,11 @@ class app(App):
 			send_button.disabled = False
 			reset_button.disabled = False
 			hue_slider.disabled = False
+			saturation_slider.disabled = False
+			cp_inverted_button.disabled = False
+			cp_grayscale_button.disabled = False
+			cp_animated_button.disabled = False
+			transform_dropdown.disabled = False
 
 
 	def build(self):
@@ -446,11 +563,25 @@ class app(App):
 
 		# FLOAT_LAYOUT.add_widget(hue_slider)
 
-		FLOAT_LAYOUT.add_widget(cp_coloured_button)
-		FLOAT_LAYOUT.add_widget(cp_grayscale_button)
 		FLOAT_LAYOUT.add_widget(cp_inverted_button)
+		FLOAT_LAYOUT.add_widget(cp_grayscale_button)
+		FLOAT_LAYOUT.add_widget(cp_animated_button)
 		FLOAT_LAYOUT.add_widget(channel_dropdown)
+
 		FLOAT_LAYOUT.add_widget(hue_slider)
+		FLOAT_LAYOUT.add_widget(hue_slider_label)
+
+		FLOAT_LAYOUT.add_widget(saturation_slider)
+		FLOAT_LAYOUT.add_widget(saturation_slider_label)
+
+		FLOAT_LAYOUT.add_widget(colour_filter_label)
+		FLOAT_LAYOUT.add_widget(invert_filter_label)
+		FLOAT_LAYOUT.add_widget(grayscale_filter_label)
+		FLOAT_LAYOUT.add_widget(animate_filter_label)
+
+		FLOAT_LAYOUT.add_widget(channel_label)
+		FLOAT_LAYOUT.add_widget(transformation_label)
+		FLOAT_LAYOUT.add_widget(transform_dropdown)
 
 
 		speed_slider.bind(value=OnSpeedSliderValueChange)
@@ -460,19 +591,28 @@ class app(App):
 		reset_button.bind(on_press = OnResetButtonPressed)
 		channel_dropdown.bind(text=OnChannelDropdownSelect)
 
+		transform_dropdown.bind(text=OnTransformDropdownSelect)
+
 		# hue_slider.bind(value=OnHueSliderValueChange)
 
 		speed_slider.disabled = True
 		send_button.disabled = True
 		reset_button.disabled = True
 		hue_slider.disabled = True
+		saturation_slider.disabled = True
+		cp_inverted_button.disabled = True
+		cp_grayscale_button.disabled = True
+		cp_animated_button.disabled = True
+		transform_dropdown.disabled = True
 
 		# Colour Preferences buttons
 
-		cp_coloured_button.bind(active=OnCPColouredButtonPressed)
-		cp_grayscale_button.bind(active=OnCPGrayscaleButtonPressed)
 		cp_inverted_button.bind(active=OnCPInvertedButtonPressed)
+		cp_grayscale_button.bind(active=OnCPGrayscaleButtonPressed)
+		cp_animated_button.bind(active=OnCPanimatedButtonPressed)
+
 		hue_slider.bind(value=OnHueSliderChange)
+		saturation_slider.bind(value=OnSaturationSliderChange)
 
 		
 		return FLOAT_LAYOUT
